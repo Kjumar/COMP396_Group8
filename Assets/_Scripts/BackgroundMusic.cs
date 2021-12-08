a5c90260018da204bba448b4f63ab42570ff6dad
Kjumar <jganguli@hotmail.ca>
2021-12-08 10:35:24 -0500

    reimported Guizukah music

---
+++ Assets/_Scripts/BackgroundMusic.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using Assets.Scripts;
+using UnityEngine;
+
+public class BackgroundMusic : MonoBehaviour
+{
+    [SerializeField]
+    Sounds[] musics;
+
+    Sounds soundScript = new Sounds();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        soundScript.LoadSounds(musics);
+        soundScript.PlayRandomSound(musics);
+
+    }
+
+
+}
