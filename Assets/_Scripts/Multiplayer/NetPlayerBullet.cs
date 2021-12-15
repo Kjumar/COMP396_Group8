a53fbac1b1708c656d35c9580ada74553b8cd5d0
Kjumar <jganguli@hotmail.ca>
2021-12-14 20:48:56 -0500

    added the handcannon to multiplayer

---
+++ Assets/_Scripts/Multiplayer/NetPlayerBullet.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetPlayerBullet : NetworkBehaviour
+{
+    public int damage = 1;
+    public float life = 3;
+
+    private void Update()
+    {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+        if (life <= 0)
+        {
+            CmdDestroy();
+        }
+
+        life -= Time.deltaTime;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+        if (other.gameObject.transform.CompareTag("Enemy"))
+        {
+            IDamageable target = other.gameObject.GetComponent<IDamageable>();
+
+            if (target != null)
+            {
+                target.TakeDamage(damage);
+            }
+        }
+
+        CmdDestroy();
+    }
+
+    [Command]
+    private void CmdDestroy()
+    {
+        NetworkServer.Destroy(gameObject);
+    }
+}
