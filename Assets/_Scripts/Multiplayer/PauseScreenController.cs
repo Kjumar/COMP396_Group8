8901b30c06224647585094f0173f7d8ba53e2952
Kjumar <jganguli@hotmail.ca>
2021-12-14 21:18:07 -0500

    added volume slier in settings menu

--- Assets/_Scripts/Multiplayer/PauseScreenController.cs
+++ Assets/_Scripts/Multiplayer/PauseScreenController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PauseScreenController : MonoBehaviour
 {
     [SerializeField] GameObject pauseCanvas;
+    [SerializeField] GameObject settingsCanvas;
 
     // Start is called before the first frame update
     void Start()
@@ -20,15 +21,29 @@ public class PauseScreenController : MonoBehaviour
             if (pauseCanvas.activeSelf)
             {
                 pauseCanvas.SetActive(false);
+                settingsCanvas.SetActive(false);
                 Cursor.lockState = CursorLockMode.Locked;
                 Cursor.visible = false;
             }
             else
             {
                 pauseCanvas.SetActive(true);
+                settingsCanvas.SetActive(false);
                 Cursor.lockState = CursorLockMode.None;
                 Cursor.visible = true;
             }
         }
+    }
+
+    public void OpenSettings()
+    {
+        pauseCanvas.SetActive(false);
+        settingsCanvas.SetActive(true);
+    }
+
+    public void CloseSettings()
+    {
+        pauseCanvas.SetActive(true);
+        settingsCanvas.SetActive(false);
     }
 }
